dd7ef57f613ab04a496b52feea36a76e009417a0
Jesica <jesica.susanto@mail.utoronto.ca>
2023-06-08 12:37:56 +0700

    add styles to industry card

--- components/IndustriesGrid.js
+++ components/IndustriesGrid.js
@@ -1,74 +1,80 @@
 import styles from './IndustriesGrid.module.css';
 
 export default function IndustriesGrid() {
+
   const gridData = [
     {
       title: 'HR',
-      checkpoints: ['1', '2', '3'],
+      checkpoints: ['Increase employee productivity', 'Streamline recruitment processes', 'Automate HR tasks'],
       buttonLabel: 'Join Waitlist',
     },
     {
       title: 'Law',
-      checkpoints: ['1', '2', '3'],
+      checkpoints: ['Improve legal document management', 'Automate contract review processes', 'Enhance legal research'],
       buttonLabel: 'Join Waitlist',
     },
     {
       title: 'Insurance',
-      checkpoints: ['Optimize your operational costs', 'Help adjusters and underwriters automate daunting tasks', 'Future-proof your insurance business with automation'],
+      checkpoints: ['Optimize operational costs', 'Automate underwriting tasks', 'Improve claims processing efficiency'],
       buttonLabel: 'Join Waitlist',
     },
     {
       title: 'Healthcare',
-      checkpoints: ['Focus on patient care, not paperwork', 'Reduce burnout-inducing administrative tasks', 'Drive positive healthcare outcomes with automation'],
+      checkpoints: ['Enhance patient care delivery', 'Automate administrative tasks', 'Improve healthcare outcomes'],
       buttonLabel: 'Join Waitlist',
     },
     {
       title: 'Freight',
-      checkpoints: ['1', '2', '3'],
+      checkpoints: ['Streamline logistics operations', 'Automate freight tracking', 'Optimize supply chain management'],
       buttonLabel: 'Join Waitlist',
     },
     {
       title: 'Pharmacy',
-      checkpoints: ['1', '2', '3'],
+      checkpoints: ['Improve medication dispensing processes', 'Enhance inventory management', 'Automate prescription verification'],
       buttonLabel: 'Join Waitlist',
     },
     {
       title: 'Customer Support',
-      checkpoints: ['1', '2', '3'],
+      checkpoints: ['Enhance customer service experience', 'Automate ticket management', 'Improve response time'],
       buttonLabel: 'Join Waitlist',
     },
     {
       title: 'Sales Development',
-      checkpoints: ['1', '2', '3'],
+      checkpoints: ['Automate lead generation', 'Improve sales forecasting', 'Enhance customer relationship management'],
       buttonLabel: 'Join Waitlist',
     },
     {
       title: 'Let us build for you',
-      checkpoints: ['1', '2', '3'],
+      checkpoints: ['Customize automation solutions', 'Tailor workflows to your needs', 'Maximize operational efficiency'],
       buttonLabel: 'Join Waitlist',
     },
     {
       title: 'Developers',
-      checkpoints: ['1', '2', '3'],
+      checkpoints: ['Simplify software development processes', 'Automate code testing', 'Accelerate project delivery'],
       buttonLabel: 'Join Waitlist',
     },
   ];
 
   return (
     <div className={styles.container}>
-                <div className={styles.row}>
-                    {gridData.map((grid, index) => (
-                      <div key={index} className={styles.card}>
-                        <h2 className={styles.title}>{grid.title}</h2>
-                        <ul className={styles.checkpoints}>
-                          {grid.checkpoints.map((checkpoint, checkpointIndex) => (
-                            <li key={checkpointIndex}>{checkpoint}</li>
-                          ))}
-                        </ul>
-                        <button className={styles.button}>{grid.buttonLabel}</button>
-                      </div>
-                    ))}
-                  </div>
-                </div>
+      <h1 className={styles.heading}>
+
+        Revolutionizing Industries with AI-First Automation for Growth and Transformation
+      </h1>
+      <div className={styles.row}>
+        {gridData.map((grid, index) => (
+          <div key={index} className={styles.card}>
+            <h2 className={styles.title}>{grid.title}</h2>
+            <ul className={styles.checkpoints}>
+              {grid.checkpoints.map((checkpoint, checkpointIndex) => (
+                <li key={checkpointIndex}>{checkpoint}</li>
+              ))}
+            </ul>
+            <button className={styles.button}>{grid.buttonLabel}</button>
+          </div>
+        ))}
+      </div>
+    </div>
+
   );
 }
