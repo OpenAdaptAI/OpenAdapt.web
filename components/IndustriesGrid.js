ce44b4b9827874dc890152476d4186e524705e04
Jesica <jesica.susanto@mail.utoronto.ca>
2023-06-09 11:32:43 +0700

    fix grid and button responsiveness

--- components/IndustriesGrid.js
+++ components/IndustriesGrid.js
@@ -5,78 +5,76 @@ export default function IndustriesGrid() {
   const gridData = [
     {
       title: 'HR',
-      checkpoints: ['Increase employee productivity', 'Streamline recruitment processes', 'Automate HR tasks'],
+      descriptions: 'Transform HR operations and boost team productivity with automation, driving strategic decision-making and talent management.',
       buttonLabel: 'Join Waitlist',
     },
     {
       title: 'Law',
-      checkpoints: ['Improve legal document management', 'Automate contract review processes', 'Enhance legal research'],
+      descriptions: 'Streamline your legal procedures and amplify case management efficacy by delegating repetitive tasks to our automation solution.',
       buttonLabel: 'Join Waitlist',
     },
     {
       title: 'Insurance',
-      checkpoints: ['Optimize operational costs', 'Automate underwriting tasks', 'Improve claims processing efficiency'],
+      descriptions: 'Optimize operational costs',
       buttonLabel: 'Join Waitlist',
     },
     {
       title: 'Healthcare',
-      checkpoints: ['Enhance patient care delivery', 'Automate administrative tasks', 'Improve healthcare outcomes'],
+      descriptions: 'Optimize patient care and medical record keeping by integrating our automation tool, freeing healthcare professionals for critical tasks.',
       buttonLabel: 'Join Waitlist',
     },
     {
       title: 'Freight',
-      checkpoints: ['Streamline logistics operations', 'Automate freight tracking', 'Optimize supply chain management'],
+      descriptions: 'Upgrade your logistics, ensure on-time deliveries and increase efficiency by automating repetitive tasks in your freight business.',
       buttonLabel: 'Join Waitlist',
     },
     {
       title: 'Pharmacy',
-      checkpoints: ['Improve medication dispensing processes', 'Enhance inventory management', 'Automate prescription verification'],
+      descriptions: 'Reinforce accuracy, patient safety and efficient inventory management in your pharmacy with our dependable automation product.',
       buttonLabel: 'Join Waitlist',
     },
     {
       title: 'Customer Support',
-      checkpoints: ['Enhance customer service experience', 'Automate ticket management', 'Improve response time'],
+      descriptions: 'Revolutionize customer service by automifying mundane tasks, enhancing response times and fostering superior customer satisfaction.',
       buttonLabel: 'Join Waitlist',
     },
     {
       title: 'Sales Development',
-      checkpoints: ['Automate lead generation', 'Improve sales forecasting', 'Enhance CRM'],
+      descriptions: 'Elevate your sales strategy by automating repetitive tasks, freeing your team to focus on strategic client engagement and revenue growth.',
       buttonLabel: 'Join Waitlist',
     },
     {
       title: 'Let us build for you',
-      checkpoints: ['Customize automation solutions', 'Tailor workflows to your needs', 'Maximize operational efficiency'],
+      descriptions: 'Customize automation solutions',
       buttonLabel: 'Join Waitlist',
     },
     {
       title: 'Developers',
-      checkpoints: ['Simplify software development processes', 'Automate code testing', 'Accelerate project delivery'],
+      descriptions: 'Simplify software development processes',
       buttonLabel: 'Join Waitlist',
     },
   ];
 
   return (
-    <div className={styles.container}>
+    <div className={styles.background}>
       <div>
         <h2 className={styles.heading}>Achieve more with less effort.</h2>
-          <p className={styles.p}>
-          Our technology observes and records your software interactions, automating repetitive tasks.<br></br>
+        <p className={styles.p}>
+          Our technology observes and records your software interactions, automating repetitive tasks.<br />
           Spend less time on manual processes and more on tasks that truly matter.
-          </p>
+        </p>
       </div>
-      <br>
-      </br>
+      <br />
       <h1 className={styles.heading}>
-
         Revolutionizing Industries with AI-First Automation for Growth and Transformation.
       </h1>
       <div className={styles.row}>
         {gridData.map((grid, index) => (
           <div key={index} className={styles.card}>
             <h2 className={styles.title}>{grid.title}</h2>
-            <ul className={styles.checkpoints}>
-              {grid.checkpoints.map((checkpoint, checkpointIndex) => (
-                <li key={checkpointIndex}>{checkpoint}</li>
+            <ul className={styles.descriptions}>
+              {grid.descriptions.split('\n').map((description) => (
+                <li>{description}</li>
               ))}
             </ul>
             <button className={styles.button}>{grid.buttonLabel}</button>
@@ -84,6 +82,5 @@ export default function IndustriesGrid() {
         ))}
       </div>
     </div>
-
   );
 }
