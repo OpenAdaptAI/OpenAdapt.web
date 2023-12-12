8bd2968ddf1d6035c6ff07bac27afdc2b6985845
Richard Abrich <richard.abrich@gmail.com>
2023-12-12 16:01:17 -0600

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -29,7 +29,7 @@ export default function Footer() {
                             <a
                                 className="font-medium text-blue-300 dark:text-blue-100 hover:underline"
                                 href="https://github.com/OpenAdaptAI/OpenAdapt#run"
-				style={{backgroundColor: '#560DF8'}
+				style={{backgroundColor: '#560DF8'}}
                             >
                                 Read our usage instructions
                             </a>
@@ -38,7 +38,7 @@ export default function Footer() {
                             <a
                                 className="font-medium text-blue-300 dark:text-blue-100 hover:underline"
                                 href="https://discord.gg/yF527cQbDG"
-				style={{backgroundColor: '#560DF8'}
+				style={{backgroundColor: '#560DF8'}}
                             >
                             Join us on Discord
                             </a>
@@ -48,7 +48,7 @@ export default function Footer() {
                         Please <a
                             className="font-medium text-blue-300 dark:text-blue-100 hover:underline"
                             href="https://github.com/OpenAdaptAI/OpenAdapt/issues/new"
-		    	    style={{backgroundColor: '#560DF8'}
+		    	    style={{backgroundColor: '#560DF8'}}
                         >
                             submit an issue to our Github
                         </a>.
