4ff9a1093c78ec72ee6661a542c59588ff2fbf6f
Richard Abrich <richard.abrich@gmail.com>
2024-03-03 18:26:11 -0500

    handleGetStartedButtonClick

--- components/IndustriesGrid.js
+++ components/IndustriesGrid.js
@@ -9,55 +9,46 @@ export default function IndustriesGrid({ feedbackData, setFeedbackData, sectionR
     {
       title: 'HR',
       descriptions: 'Boost team productivity in HR operations. Automate candidate sourcing using LinkedIn Recruiter, LinkedIn Talent Solutions, GetProspect, Reply.io, outreach.io, Gmail/Outlook, and more.',
-      buttonLabel: 'Join Waitlist',
       logo: '/images/noun-human-resources.svg',
     },
     {
       title: 'Law',
       descriptions: 'Streamline legal procedures and case management. Automate tasks like generating legal documents, managing contracts, tracking cases, and conducting legal research with LexisNexis, Westlaw, Adobe Acrobat, Microsoft Excel, and more.',
-      buttonLabel: 'Join Waitlist',
       logo: '/images/noun-law.svg',
     },
     {
       title: 'Insurance',
       descriptions: 'Optimize productivity in insurance. Automate policy management, claims processing, data analysis, and document collaboration with PolicyCenter, Xactimate, Excel, SharePoint, PowerBI, and more.',
-      buttonLabel: 'Join Waitlist',
       logo: '/images/noun-insurance.svg',
     },
     {
       title: 'Healthcare',
       descriptions: 'Advance patient care and streamline operations. Automate revenue cycle management, clinical documentation, and scheduling in Cerner, Epic, and more.',
-      buttonLabel: 'Join Waitlist',
       logo: '/images/noun-healthcare.svg',
     },
     {
       title: 'Logistics',
       descriptions: 'Automate tasks with Transportation Management Systems (TMS), Freight Management Systems (FMS), Load Tracking Systems, and Document Management Systems for efficient tracking, scheduling, and financial record-keeping.',
-      buttonLabel: 'Join Waitlist',
       logo: '/images/noun-freight.svg',
     },
     {
       title: 'Pharmacy',
       descriptions: 'Enhance accuracy and inventory management. Automate prescription management, inventory control, medication dispensing, and patient records with Krol (Telus), Filware, Healthwatch, and more.',
-      buttonLabel: 'Join Waitlist',
       logo: '/images/noun-pharmacy.svg',
     },
     {
       title: 'Customer Support',
       descriptions: 'Automate customer inquiries, ticket management, collaboration, data analysis, and communication using OracleHCM, Workday, SAP, Excel, SharePoint, Outlook, LinkedIn, Teams, PowerBI, and more.',
-      buttonLabel: 'Join Waitlist',
       logo: '/images/noun-customer-support.svg',
     },
     {
       title: 'Sales Development',
       descriptions: 'Automate repetitive tasks in OracleHCM, LinkedIn, SalesForce, and Gmail for lead generation, prospecting, and communication to optimize revenue growth.',
-      buttonLabel: 'Join Waitlist',
       logo: '/images/noun-sales-development.svg',
     },
     {
       title: 'Let us build for you',
       descriptions: "If OpenAdapt doesn't fully automate your workflow out of the box, we'll work with you to fix that.",
-      buttonLabel: 'Join Waitlist',
       logo: '/images/noun-build.svg',
     },
   ];
@@ -68,14 +59,23 @@ export default function IndustriesGrid({ feedbackData, setFeedbackData, sectionR
     }
   };
 
-  const handleButtonClick = (title) => {
-    let data = {
+  const getDataFromTitle = (title) => {
+    return {
       email: '',
       message: title === 'Let us build for you' ? '' : `I'm interested in how OpenAdapt can help me make ${title} better.`
     };
-    setFeedbackData(data);
-    scrollToSection();
+  }
+
+  const handleWaitlistButtonClick = (title) => {
+      let data = getDataFromTitle(title);
+      setFeedbackData(data);
+      scrollToSection();
   };
+
+  const handleGetStartedButtonClick = (title) => {
+      let data = getDataFromTitle(title);
+      setFeedbackData(data);
+  }
 
   return (
     <div className={styles.background} id="industries">
@@ -108,10 +108,10 @@ export default function IndustriesGrid({ feedbackData, setFeedbackData, sectionR
               ))}
             </ul>
             <div className="flex flex-row align-items-center justify-content-center mt-2 mb-4">
-                <button className={`btn btn bg-transparent border-2 border-blue-400 text-blue-400 hover:border-blue-300 hover:text-blue-300 ${styles.button}`} onClick={() => handleButtonClick(grid.title)}>
-                    {grid.buttonLabel}
+                <button className={`btn btn bg-transparent border-2 border-blue-400 text-blue-400 hover:border-blue-300 hover:text-blue-300 ${styles.button}`} onClick={() => handleWaitlistButtonClick(grid.title)}>
+                    Join Waitlist
                 </button>
-                <Link className="btn btn-primary ml-2" href="#developers">
+                <Link className="btn btn-primary ml-2" href="#developers" onClick={() => handleGetStartedButtonClick(grid.title)}>
                     Get Started
                 </Link>
             </div>
