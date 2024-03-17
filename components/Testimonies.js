df1c8a5989d4d25ce9777a6eccbb74a224af3c70
Yassa Taiseer <yassataiseer@gmail.com>
2024-03-17 17:00:36 -0400

    implemented smoother animations and now title position is alays centered

--- components/Testimonies.js
+++ components/Testimonies.js
@@ -30,25 +30,12 @@ export default function Testimonies() {
             Thank you again for making OpenAdapter`,
             author:`Ribeam Kim`
         },
-        /*{
-            text: `Great AI Work. Fixed the issues in my ship and now me and Chewbacca can race the galaxy! Vroom 🚀`,
-            author: `Han Solo, space person`
-        },
-        {
-            text: `As a small business owner, managing billing discrepancies was an ongoing challenge until I found OpenAdapt. Unbeknownst to us, we were missing out on $40K in procedural RVUs. The manual effort to recover it was daunting. OpenAdapt simplified the entire process with its automated solution, saving me valuable time and resources. The team's personalized assistance made the transition seamless. I highly recommend OpenAdapt for anyone seeking efficiency in revenue recovery.`,
-            author: `Emily Thompson`
-        },
-        {
-            text: `OpenAdapt revolutionized the way we approach procedural audits. Before using their platform, identifying $60K in under-billed RVUs was a tedious task. With OpenAdapt, it became an effortless journey, completed in a fraction of the time. The user-friendly interface and exceptional customer support exceeded my expectations. I'm now a loyal advocate, incorporating OpenAdapt into our monthly auditing routine for sustained financial accuracy.`,
-            author: `Robert Davis`
-        }, {
-            text: `Managing procedural RVUs had become a headache until OpenAdapt came to the rescue. Our oversight led to $55K in lost revenue, and manual recovery efforts were eating into our time. OpenAdapt's automated solution streamlined the process, recovering the under-billed amount with ease. The personalized support provided by the OpenAdapt team ensured a smooth transition. I'm impressed with the results and will continue relying on OpenAdapt for our monthly audits.`,
-            author: `Robert Davis`
-        },*/
     ]
     return (
+        <div className="testamonialRow">
+        <h1 className={styles.header}>Some Testimonies</h1>
         <div className={styles.card_box}>
-            <h1 className={styles.header}>Some Testimonies</h1>
+            
             {TestimonyData.map((curr_testimony) => (
                 <div className={styles.card}>
                     <Image
@@ -64,6 +51,7 @@ export default function Testimonies() {
                     <h2 className={styles.title}>   <hr></hr> — {curr_testimony.author}</h2>
                 </div>
             ))}
+        </div>
         </div>
     )
 }
