e88c1c6f6006ce289ff6c266ae16ce19896ed3b8
Richard Abrich <richard.abrich@gmail.com>
2024-06-12 14:44:04 -0400

    / -> /form.html

--- components/FeedbackForm.js
+++ components/FeedbackForm.js
@@ -26,7 +26,7 @@ export default function FeedbackForm({ feedbackData }) {
         const formData = new FormData(event.target)
         formData.append('form-name', 'feedback')
 
-        fetch('/', {
+        fetch('/form.html', {
             method: 'POST',
             headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
             body: new URLSearchParams(formData).toString(),
