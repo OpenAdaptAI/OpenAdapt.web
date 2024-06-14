504cbcc8124018936e1e29ffd3751d5c01336fd9
Richard Abrich <richard.abrich@gmail.com>
2024-06-14 00:07:11 -0400

    Update Developers.js: warning style

--- components/Developers.js
+++ components/Developers.js
@@ -77,14 +77,14 @@ export default function Developers() {
         <div className={styles.row} id="start">
             <div className="relative flex items-center justify-center mx-20 md-12">
                 <div className="grid grid-cols-1 break-words">
-                    {showBuildWarning && (
-                        <div className="bg-red-500 text-white text-center p-4">
-                            Warning: The current version has a known issue and may not function as expected. Please check back later for updates.
-                        </div>
-                    )}
                     <h2 id="start" className="text-2xl mt-10 mb-5 font-light text-center">
                         Getting Started
                     </h2>
+                    {showBuildWarning && (
+                        <div className="bg-yellow-600 text-white text-center p-4">
+                            Warning: The current version has a known issue and may not function as expected. Please check back later for updates.
+                        </div>
+                    )}
                     <p className="font-light mb-6 mx-auto">
                         Note: OpenAdapt is Alpha software. This means it is in
                         the early stages of development and may contain bugs,
