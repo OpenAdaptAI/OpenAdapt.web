94529d3e335ae8fe82f4f0d59b9835f566d39960
Richard Abrich <richard.abrich@gmail.com>
2024-06-16 19:39:20 -0400

    Update Developers.js: move What's Next after download links

--- components/Developers.js
+++ components/Developers.js
@@ -165,70 +165,6 @@ export default function Developers() {
                         className="mx-auto mb-4"
                     ></iframe>
                     <EmailForm />
-                    <p className="text-center">Comfortable on the command line? Read on:</p>
-                    <h2 id="start" className="text-2xl mt-10 font-light mb-5">
-                        Getting Started for Developers
-                    </h2>
-                    <p>
-                        Note: If you run into trouble with the scripted
-                        installation described here, please refer to{' '}
-                        <a href="https://github.com/OpenAdaptAI/OpenAdapt?tab=readme-ov-file#manual-setup">
-                            Manual Setup
-                        </a>{' '}
-                        instructions.
-                    </p>
-                    <h3 className="my-3">
-                        <FontAwesomeIcon
-                            icon={faWindows}
-                            className="text-3xl mr-4"
-                        />
-                        <b>Windows</b>
-                    </h3>
-                    <ul className="mt-2 list-disc list-inside font-light">
-                        <li>
-                            Press Windows Key, type "powershell", and press
-                            Enter
-                        </li>
-                        <li>
-                            Copy and paste the following command into the
-                            terminal, and press Enter (If Prompted for `User
-                            Account Control`, click 'Yes'):
-                        </li>
-                    </ul>
-                    <pre
-                        className="whitespace-pre-wrap code text-slate-600 bg-slate-100 p-3 m-2"
-                        style={{ backgroundColor: '#f8f6ff' }}
-                    >
-                        Start-Process powershell -Verb RunAs -ArgumentList
-                        '-NoExit', '-ExecutionPolicy', 'Bypass', '-Command',
-                        "iwr -UseBasicParsing -Uri
-                        'https://raw.githubusercontent.com/OpenAdaptAI/OpenAdapt/main/install/install_openadapt.ps1'
-                        | Invoke-Expression"
-                    </pre>
-                    <h3 className="my-3">
-                        <FontAwesomeIcon
-                            icon={faApple}
-                            className="text-3xl mr-4"
-                        />
-                        <b>MacOS</b>
-                    </h3>
-                    <ul className="mt-2 list-disc list-inside font-light">
-                        <li>
-                            Press Command+Space, type "terminal", and press
-                            Enter
-                        </li>
-                        <li>
-                            Copy and paste the following command into the
-                            terminal, and press Enter:
-                        </li>
-                    </ul>
-                    <pre
-                        className="whitespace-pre-wrap code text-slate-600 bg-slate-100 p-3 m-2"
-                        style={{ backgroundColor: '#f8f6ff' }}
-                    >
-                        /bin/bash -c "$(curl -fsSL
-                        https://raw.githubusercontent.com/OpenAdaptAI/OpenAdapt/HEAD/install/install_openadapt.sh)"
-                    </pre>
                     <h2 className="text-2xl mt-10 font-light">What's Next?</h2>
                     <ul className={`${styles.noBullets} mt-2 font-light`}>
                         <li className="mt-2 font-light">
@@ -297,6 +233,70 @@ export default function Developers() {
                             </a>
                         </li>
                     </ul>
+                    <p className="text-center">Comfortable on the command line? Read on:</p>
+                    <h2 id="start" className="text-2xl mt-10 font-light mb-5">
+                        Getting Started for Developers
+                    </h2>
+                    <p>
+                        Note: If you run into trouble with the scripted
+                        installation described here, please refer to{' '}
+                        <a href="https://github.com/OpenAdaptAI/OpenAdapt?tab=readme-ov-file#manual-setup">
+                            Manual Setup
+                        </a>{' '}
+                        instructions.
+                    </p>
+                    <h3 className="my-3">
+                        <FontAwesomeIcon
+                            icon={faWindows}
+                            className="text-3xl mr-4"
+                        />
+                        <b>Windows</b>
+                    </h3>
+                    <ul className="mt-2 list-disc list-inside font-light">
+                        <li>
+                            Press Windows Key, type "powershell", and press
+                            Enter
+                        </li>
+                        <li>
+                            Copy and paste the following command into the
+                            terminal, and press Enter (If Prompted for `User
+                            Account Control`, click 'Yes'):
+                        </li>
+                    </ul>
+                    <pre
+                        className="whitespace-pre-wrap code text-slate-600 bg-slate-100 p-3 m-2"
+                        style={{ backgroundColor: '#f8f6ff' }}
+                    >
+                        Start-Process powershell -Verb RunAs -ArgumentList
+                        '-NoExit', '-ExecutionPolicy', 'Bypass', '-Command',
+                        "iwr -UseBasicParsing -Uri
+                        'https://raw.githubusercontent.com/OpenAdaptAI/OpenAdapt/main/install/install_openadapt.ps1'
+                        | Invoke-Expression"
+                    </pre>
+                    <h3 className="my-3">
+                        <FontAwesomeIcon
+                            icon={faApple}
+                            className="text-3xl mr-4"
+                        />
+                        <b>MacOS</b>
+                    </h3>
+                    <ul className="mt-2 list-disc list-inside font-light">
+                        <li>
+                            Press Command+Space, type "terminal", and press
+                            Enter
+                        </li>
+                        <li>
+                            Copy and paste the following command into the
+                            terminal, and press Enter:
+                        </li>
+                    </ul>
+                    <pre
+                        className="whitespace-pre-wrap code text-slate-600 bg-slate-100 p-3 m-2"
+                        style={{ backgroundColor: '#f8f6ff' }}
+                    >
+                        /bin/bash -c "$(curl -fsSL
+                        https://raw.githubusercontent.com/OpenAdaptAI/OpenAdapt/HEAD/install/install_openadapt.sh)"
+                    </pre>
                 </div>
             </div>
         </div>
