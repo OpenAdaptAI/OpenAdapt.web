73109d2f64e0b3b06696432c569e101dd5162da5
Richard Abrich <richard.abrich@gmail.com>
2024-06-16 09:54:59 -0400

    add DownloadStats.js

--- components/Developers.js
+++ components/Developers.js
@@ -4,6 +4,8 @@ import { faWindows, faApple } from '@fortawesome/free-brands-svg-icons';
 import Link from 'next/link';
 import styles from './Developers.module.css';
 import { getReleasesDownloadCount } from 'utils/githubStats';
+import DownloadStats from './DownloadStats';
+
 
 export default function Developers() {
     const [latestRelease, setLatestRelease] = useState({ version: null, date: null });
@@ -162,6 +164,7 @@ export default function Developers() {
                         style={{ border: '0', borderRadius: '6px' }}
                         className="mx-auto mb-4"
                     ></iframe>
+                    <DownloadStats />
                     <p className="text-center">
                         Don't forget to{' '}
                         <a href="#register">Register for Updates</a>.
